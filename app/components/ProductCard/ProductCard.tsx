e90e58c9b528056a22fed97e8072f972d11e9132
Md. Abu Musa <musa.fci@gmail.com>
2023-10-16 01:30:24 +0600

    Use tailwind css to design add to cart button

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import AddToCart from '../AddToCart';
-import Style from './ProductCard.module.css';
 
 const ProductCard = () => {
   return (
-    <div className={Style.card}>
+    <div className='p-4 my-4 bg-sky-300 font-bold text-white text-center hover:bg-sky-500'>
         <AddToCart/>
     </div>
   )
