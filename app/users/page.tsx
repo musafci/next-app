540295287091765fee8c3b4dfa7312755857172d
Md. Abu Musa <musa.fci@gmail.com>
2023-10-15 15:40:23 +0600

    Caching apply on fetch() functon.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,7 +6,17 @@ interface User {
 }
 
 const users = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    // const response = await fetch(
+    //     'https://jsonplaceholder.typicode.com/users',
+    //     { cache: 'no-store' }) //To disable caching, and this is usefull if data that changes frequently
+
+    const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users',
+        { next: { revalidate: 10 }  }) //This means next.js is going to run a background job and get fresh data from the backend every 10 second
+
+    // NB:: This caching behavior only implemented in the fetch function, others third party library, you are not going to get the data cache.
+    
     const users: User[] = await response.json();
 
   return (
