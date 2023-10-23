f688fda57a37641275119550d805fb5ad05a9ce5
Md. Abu Musa <musa.fci@gmail.com>
2023-10-23 09:18:16 +0600

    Programmatic Navigation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import UserTable from './UserTable'
+import Link from 'next/link';
 
 interface PropsData {
     searchParams : {sortOrder : string};
@@ -10,6 +11,7 @@ const users = async ({searchParams: {sortOrder}} : PropsData) => {
   return (
     <>
         <h1>Users</h1>
+        <Link href="/users/new" className='btn'>Add User</Link>
         <UserTable sortOrder={sortOrder}/>
     </>
   )
